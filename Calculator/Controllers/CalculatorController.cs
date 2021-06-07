b5b115dd23d349e351fd731e225bb40e027c31f5
Sudhakar-sk <msd30499@gmail.com>
2021-06-07 15:17:23 +0530

    Add project files.

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Calculate()
+        {
+            return View();
+        }
+
+        
+    }
+}
